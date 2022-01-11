2116f9d8140d9ff24ec4bc541f1bc9eef9f8643a
Navjot Kukreja <navjotk@gmail.com>
2022-01-11 17:33:17 -0300

    Fix flake errors

--- setup.py
+++ setup.py
@@ -4,20 +4,23 @@ from setuptools.command.build_ext import build_ext
 from distutils import log as distutils_logger
 from distutils.errors import DistutilsSetupError
 
-import os, subprocess
-import glob
+import os
+import subprocess
 import setuptools
+
+ZFP_DOWNLOAD_PATH = 'https://github.com/LLNL/zfp/releases/download/0.5.5/zfp-0.5.5.tar.gz'  # noqa
 
 
 def download_file(url):
     import requests
     fname = url.split("/")[-1]
     r = requests.get(url)
-    with open(fname , 'wb') as f:
+    with open(fname, 'wb') as f:
         f.write(r.content)
-    
-# check whether compiler supports a flag
+
+
 def has_flag(compiler, flagname):
+    """Check whether a flag is supported on a compiler."""
     import tempfile
     from distutils.errors import CompileError
     with tempfile.NamedTemporaryFile('w', suffix='.cpp') as f:
@@ -29,8 +32,8 @@ def has_flag(compiler, flagname):
     return True
 
 
-# filter flags, returns list of accepted flags
 def flag_filter(compiler, *flags):
+    """Filter flags, returns list of accepted flags"""
     result = []
     for flag in flags:
         if has_flag(compiler, flag):
@@ -62,14 +65,15 @@ def extensions():
     import numpy
     from Cython.Build import cythonize
     ext = Extension("pyzfp",
-              sources=["pyzfp.pyx"],
-              include_dirs=['zfp-0.5.5/include',
-                            numpy.get_include()],
-              libraries=["zfp"],  # Unix-like specific,
-              library_dirs=["zfp-0.5.5/lib"],
-             #extra_link_args=['-Wl,-rpath,/usr/local/lib']
-              )
+                    sources=["pyzfp.pyx"],
+                    include_dirs=['zfp-0.5.5/include',
+                                  numpy.get_include()],
+                    libraries=["zfp"],  # Unix-like specific,
+                    library_dirs=["zfp-0.5.5/lib"],
+                    # extra_link_args=['-Wl,-rpath,/usr/local/lib']
+                    )
     return cythonize([ext])
+
 
 class specialized_build_ext(build_ext, object):
     """
@@ -87,8 +91,8 @@ class specialized_build_ext(build_ext, object):
             clang = False
         else:
             clang = True
-            
-        if ext.name!=self.special_extension:
+
+        if ext.name != self.special_extension:
             # Handle unspecial extensions with the parent class' method
             super(specialized_build_ext, self).build_extension(ext)
         else:
@@ -100,21 +104,22 @@ class specialized_build_ext(build_ext, object):
                        "'sources' must be present and must be "
                        "a list of source filenames" % ext.name)
             sources = list(sources)
-            if len(sources)>1:
+            if len(sources) > 1:
                 sources_path = os.path.commonpath(sources)
             else:
                 sources_path = os.path.dirname(sources[0])
             sources_path = os.path.realpath(sources_path)
             if not sources_path.endswith(os.path.sep):
-                sources_path+= os.path.sep
+                sources_path += os.path.sep
 
-            if not os.path.exists(sources_path) or not os.path.isdir(sources_path):
+            if not os.path.exists(sources_path) or \
+               not os.path.isdir(sources_path):
                 raise DistutilsSetupError(
                        "in 'extensions' option (extension '%s'), "
                        "the supplied 'sources' base dir "
                        "must exist" % ext.name)
 
-            download_file('https://github.com/LLNL/zfp/releases/download/0.5.5/zfp-0.5.5.tar.gz')
+            download_file(ZFP_DOWNLOAD_PATH)
             command = 'make'
             if clang:
                 command += ' OPENMP=0'
@@ -126,22 +131,26 @@ class specialized_build_ext(build_ext, object):
             for v in env_vars:
                 val = os.getenv(v)
                 if val is not None:
-                    command += ' %s=%s'%(v, val)
+                    command += ' %s=%s' % (v, val)
 
-            distutils_logger.info('Will execute the following command in with subprocess.Popen: \n{0}'.format(command))
+            distutils_logger.info('Will execute the following command in ' +
+                                  'with subprocess.Popen:' +
+                                  '\n{0}'.format(command))
             try:
                 output = subprocess.check_output(command,
-                                            cwd=sources_path,
-                                            stderr=subprocess.STDOUT,
-                                            shell=True)
+                                                 cwd=sources_path,
+                                                 stderr=subprocess.STDOUT,
+                                                 shell=True)
             except subprocess.CalledProcessError as e:
                 distutils_logger.info(str(e.output))
                 raise
 
             distutils_logger.info(str(output))
 
-            # After making the library build the c library's python interface with the parent build_extension method
+            # After making the library build the c library's python interface
+            # with the parent build_extension method
             super(specialized_build_ext, self).build_extension(ext)
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -156,7 +165,7 @@ configuration = {
     'cmdclass': {'build_ext': specialized_build_ext},
     'description': "A python wrapper for the ZFP compression libary",
     'long_description': long_description,
-    'long_description_content_type':'text/markdown',
+    'long_description_content_type': 'text/markdown',
     'url': 'https://github.com/navjotk/pyzfp',
     'author': "Navjot Kukreja",
     'author_email': 'navjotk@gmail.com',
